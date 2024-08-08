2b758bc1485d8b8ce3376ea1ba648b1768ed686d
Patryk Filip Gryz <pfgryz@gmail.com>
2024-08-08 19:16:19 +0200

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -14,10 +14,10 @@ export GST_PLUGIN_PATH=/gst-plugins-rs/target/release:$GST_PLUGIN_PATH
     # 3. Run python script for detecting cameras
 (trap 'kill 0' SIGINT EXIT; 
     (
-        WEBRTCSINK_SIGNALLING_SERVER_LOG=debug ./gst-plugins-rs/target/release/gst-webrtc-signalling-server ; kill 0
+        WEBRTCSINK_SIGNALLING_SERVER_LOG=debug ./gst-plugins-rs/target/release/gst-webrtc-signalling-server --cert /certificates/firo.p12 --cert-password skar ; kill 0
     ) &
     (
-        gst-launch-1.0 webrtcsink name=ws meta="meta,name=gst-stream" signaller::uri="wss://localhost:8443" videotestsrc ! ws. audiotestsrc ! ws. ; kill 0
+        gst-launch-1.0 webrtcsink name=ws meta="meta,name=gst-stream" signaller::uri="wss://localhost:8443" signaller::cafile="/certificates/RootCA.pem" videotestsrc ! ws. audiotestsrc ! ws. ; kill 0
     ) &
     (
         python3 pipelines.py ; kill 0
