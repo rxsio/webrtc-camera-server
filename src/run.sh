1a51c36dcda2c590ed785444a03c44a5978c3b65
Patryk Filip Gryz <pfgryz@gmail.com>
2024-08-08 18:59:58 +0200

    Update run.sh
    Remove certificate

--- src/run.sh
+++ src/run.sh
@@ -2,9 +2,6 @@
 
 #region Prepare
 
-    # Activate NVM
-# source /usr/local/nvm/nvm.sh
-
     # Export GST_PLUGIN_PATH
 export GST_PLUGIN_PATH=/gst-plugins-rs/target/release:$GST_PLUGIN_PATH
 
@@ -17,10 +14,10 @@ export GST_PLUGIN_PATH=/gst-plugins-rs/target/release:$GST_PLUGIN_PATH
     # 3. Run python script for detecting cameras
 (trap 'kill 0' SIGINT EXIT; 
     (
-        WEBRTCSINK_SIGNALLING_SERVER_LOG=debug ./gst-plugins-rs/target/release/gst-webrtc-signalling-server --cert /certificates/firo.p12 --cert-password skar ; kill 0
+        WEBRTCSINK_SIGNALLING_SERVER_LOG=debug ./gst-plugins-rs/target/release/gst-webrtc-signalling-server ; kill 0
     ) &
     (
-        gst-launch-1.0 webrtcsink name=ws meta="meta,name=gst-stream" signaller::uri="wss://localhost:8443" signaller::cafile="/certificates/RootCA.pem" videotestsrc ! ws. audiotestsrc ! ws. ; kill 0
+        gst-launch-1.0 webrtcsink name=ws meta="meta,name=gst-stream" signaller::uri="ws://localhost:8443" videotestsrc ! ws. audiotestsrc ! ws. ; kill 0
     ) &
     (
         python3 pipelines.py ; kill 0
