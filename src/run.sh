7890668698a63073d04c9000322d8b7fb8ff38fc
Patryk Filip Gryz <pfgryz@gmail.com>
2024-08-16 16:33:29 +0200

    Update run script

--- src/run.sh
+++ src/run.sh
@@ -2,9 +2,6 @@
 
 #region Prepare
 
-    # Activate NVM
-source /usr/local/nvm/nvm.sh
-
     # Export GST_PLUGIN_PATH
 export GST_PLUGIN_PATH=/gst-plugins-rs/target/release:$GST_PLUGIN_PATH
 
@@ -13,14 +10,10 @@ export GST_PLUGIN_PATH=/gst-plugins-rs/target/release:$GST_PLUGIN_PATH
 #region Main loop
 
     # 1. Run signalling server
-    # 2. Run example video
-    # 3. Run python script for detecting cameras
+    # 2. Run python script for detecting cameras
 (trap 'kill 0' SIGINT EXIT; 
     (
         WEBRTCSINK_SIGNALLING_SERVER_LOG=debug ./gst-plugins-rs/target/release/gst-webrtc-signalling-server --cert /certificates/firo.p12 --cert-password skar ; kill 0
-    ) &
-    (
-        gst-launch-1.0 webrtcsink name=ws meta="meta,name=gst-stream" signaller::uri="wss://localhost:8443" signaller::cafile="/certificates/RootCA.pem" videotestsrc ! ws. audiotestsrc ! ws. ; kill 0
     ) &
     (
         python3 pipelines.py ; kill 0
